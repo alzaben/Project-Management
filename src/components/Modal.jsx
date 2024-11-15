1c8d889759fe295de59af9f96fd50fc5adc016ee
Obaida Alzaben <obaidahatem100@gmail.com>
2024-11-15 20:52:02 +0300

    Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   const dialog = useRef();
   useImperativeHandle(ref, () => {
     return {
@@ -11,6 +11,14 @@ const Modal = forwardRef(function Modal({ children }, ref) {
     };
   });
 
-  return createPortal(<dialog ref={dialog}>{children}</dialog>, document.getElementById("modal-root"));
+  return createPortal(
+    <dialog ref={dialog}>
+      {children}
+      <from method='dialog'>
+        <button>{buttonCaption}</button>
+      </from>
+    </dialog>,
+    document.getElementById("modal-root")
+  );
 });
 export default Modal;
